5aad0804fa1a2ba21187a4ba48a7e02eefa7cf3a
Joe Modjeski <jmodjeski@demark.com>
2020-11-10 15:35:10 -0700

    chore(deps): adding lock files

--- exercises/react/README.md
+++ exercises/react/README.md
@@ -9,6 +9,6 @@ This is a list of things that need to happen when the user presses "Submit order
 3. The remove ingredient buttons need to disappear.
 4. The "Number of completed orders" needs to update to reflect the correct number.
 
-After making the sumit order button work, please look at `src/components/Order.js`. What are some ways in which this component could be improved?
+After making the submit order button work, please look at `src/components/Order.js`. What are some ways in which this component could be improved?
 
 Also look at the reducer function in `src/store/orders.js`. Do you see any problems with this reducer function?
--- exercises/react/src/store/orders.js
+++ exercises/react/src/store/orders.js
@@ -35,7 +35,7 @@ const initialState = {
   '849d9334-d1c2-4847-a027-3a8cbd6ed990': {
     id: '849d9334-d1c2-4847-a027-3a8cbd6ed990',
     orderedAt: null,
-    recipientName: 'Eric',
+    recipientName: 'Dom',
     ingredients: [
       {id: '5990e4bf-1330-46f1-9766-81f033c3c826', name: 'Pepperoni'},
       {id: 'fd7c4583-129c-469b-909d-8e68a45a9e31', name: 'Sausage'},
@@ -47,7 +47,7 @@ const initialState = {
   '793cfceb-7f12-486e-8666-4eed1cbc6b41': {
     id: '793cfceb-7f12-486e-8666-4eed1cbc6b41',
     orderedAt: null,
-    recipientName: 'Joe',
+    recipientName: 'Tad',
     ingredients: [
       {id: 'd1d2a6a4-f9ad-430d-8904-3a968bf29a66', name: 'Spinach'},
       {id: '5990e4bf-1330-46f1-9766-81f033c3c826', name: 'Pepperoni'},
